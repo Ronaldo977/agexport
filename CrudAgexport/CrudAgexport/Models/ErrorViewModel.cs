d23d42cdee3788b8500d659db0daee8d8c4a6d87
Ronaldo Florian <ronaldoflorian0097@gmail.com>
2023-11-27 02:47:14 -0600

    Actualizar

--- CrudAgexport/CrudAgexport/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CrudAgexport.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Pruebas ADO sql/ADO sql/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ADO_sql.MvcApplication" Language="C#" %>
